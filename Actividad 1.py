633834471bf11013741a4a420c2c4b30ba0ad8b6
OtaloraSamuel <samuelotalora54@gmail.com>
2026-04-08 12:50:06 -0500

    Actividades

---
+++ Actividad 1.py
@@ -0,0 +1,25 @@
+
+#Ejercicio #1
+print(f'La suma es {3+4}')
+print(f'La resta es {3-4}')
+print(f'La multiplicación es {3*4}')
+print(f'Mod es {3%4}')
+print(f'La división es {3/4}')
+print(f'La potencia es {3**4}')
+print(f'El divisor es {3//4}')
+
+#Ejercicio#2
+print("Samuel")
+print("Otálora")
+print("Ecuador")
+print("Estoy disfrutando de 30 días de Python")
+
+#Ejercicio #3
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Samuel"))
+print(type("Otálora"))
+print(type("Ecuador"))
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # que-estres
+fewfwefewfwf

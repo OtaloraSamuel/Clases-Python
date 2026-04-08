77a53ea04b93222848487a9b8e15c78cb633314a
OtaloraSamuel <samuelotalora54@gmail.com>
2026-04-08 13:23:22 -0500

    Actividad 2

---
+++ Actividad_2_Variables.py
@@ -0,0 +1,7 @@
+primer_nombre, primer_apellido, pais, edad, esta_casado = 'Samuel', 'Otálora', 'Ecuador', '16', 'Casado'
+print(primer_nombre, primer_apellido, pais, edad, esta_casado)
+print(f'Primer nombre {primer_nombre}')
+print(f'Primer apéllido {primer_apellido}')
+print(f'Pais {pais}')
+print(f'Edad {edad}')
+print(f'Estado civil {esta_casado}')
--- Variables.py
+++ Variables.py
@@ -1,2 +1,52 @@
-##nada por aqui
+import yfinance
+ticker=input("Digita el código de la acción: ")
+data=yfinance.Ticker(ticker).history("6mo")
+cierre=data.Close
+#cierre.plot()
+maxima=round(cierre.max(),2)
+minima=round(cierre.min(),2)
+valor_medio=round(cierre.mean(),2)
 
+import pyautogui
+import pyperclip
+import webbrowser
+import time
+
+destinatario="erick.chiguano2020@gmail.com"
+asunto="Análisis acciones últimos 6 meses"
+mensaje=f"""
+Hola erick,
+
+Acá te envío el análisis de las acciones de los últimos 6 meses de {ticker}:
+
+Cotización máxima: USD {maxima}
+Cotización minima: USD {minima}
+Valor medio: USD {valor_medio}
+
+Hablamos!
+
+Samuel,
+"""
+
+webbrowser.open("https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox")
+
+time.sleep(3)
+
+
+
+pyautogui.click(78,170)
+time.sleep(1)
+pyperclip.copy(destinatario)
+time.sleep(1)
+pyautogui.hotkey("ctrl","v")
+pyautogui.hotkey("tab")
+
+pyperclip.copy(asunto)
+pyautogui.hotkey("ctrl","v")
+pyautogui.hotkey("tab")
+
+pyperclip.copy(mensaje)
+pyautogui.hotkey("ctrl","v")
+pyautogui.hotkey("ctrl","enter")
+time.sleep(2)
+pyautogui.hotkey("ctrl","f4")
